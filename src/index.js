b23e1a8c3160fc0e2471f3576b8ab595b3dd5b45
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 17:16:50 +0200

    updating deps

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import VanillaContextMenu from 'vanilla-context-menu'
 // const VanillaContextMenu = require('vanilla-context-menu')
 new VanillaContextMenu({
     scope: document.querySelector("body"),
-    customThemeClass: "testClassTheme",
     menuItems: [
       {
         label: 'Copy',

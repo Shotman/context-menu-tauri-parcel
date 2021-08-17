349e0191c40d1cd551c9c02c840620df5c1d356b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-17 09:54:07 +0200

    update to latest tauri beta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import VanillaContextMenu from 'vanilla-context-menu'
+// const VanillaContextMenu = require('vanilla-context-menu')
 new VanillaContextMenu({
     scope: document.querySelector("body"),
+    customThemeClass: "testClassTheme",
     menuItems: [
       {
         label: 'Copy',
